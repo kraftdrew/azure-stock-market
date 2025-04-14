97ac67c6a17809eb8c11c79bfad7a1a4e52e6a88
Andrew_mac <andrewkravchuk97@gmail.com>
2025-04-13 23:30:29 -0600

    Initial Delta Lake Dev

--- Python Helper/dev_spark_session.py
+++ Python Helper/dev_spark_session.py
@@ -1,13 +1,12 @@
 import os
 from pyspark.sql import SparkSession
+from delta import *
 
 class DevSparkSession:
     
     def __init__(self, appname: str = "SparkDev"):
         
         self.appname = appname
-
-
 
         # Set the local IP to avoid loopback hostname warnings
         os.environ["SPARK_LOCAL_IP"] = "10.0.0.131"
@@ -19,9 +18,21 @@ class DevSparkSession:
 
         # print("Using log4j.properties path:", log4j_path)
 
-        self.spark = SparkSession.builder \
+        self.spark = configure_spark_with_delta_pip(SparkSession.builder \
             .appName(self.appname) \
             .master("local[*]") \
             .config("spark.driver.host", "10.0.0.131") \
             .config("spark.driver.extraJavaOptions", f"-Dlog4j.configuration=file://{log4j_path}") \
-            .getOrCreate()  
+            .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension") \
+            .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog")) \
+            .getOrCreate() 
+            
+            
+    
+
+            
+# spark = DevSparkSession().spark
+# from delta.tables import DeltaTable
+
+
+
